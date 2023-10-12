9fd2086da575c99459fcdcd012bacd810447b238
LeonardoAlmds <leonardoalmd8@outlook.com>
2023-10-12 16:13:08 -0300

    boletim professor

--- resources/views/professor/boletim/add_boletim_professor.blade.php
+++ resources/views/professor/boletim/add_boletim_professor.blade.php
@@ -8,6 +8,7 @@
     <!-- Link para o arquivo CSS de estilo específico para o cadastro -->
     <link rel="stylesheet" href="/css/tabela_horario_cardapio.css">
     <link rel="stylesheet" href="/css/navbar.css">
+    <link rel="stylesheet" href="/css/professor/add_boletim_professor.css">
     <!-- Link para a fonte 'Noto Sans' do Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -39,6 +40,73 @@
         <h1>Adicionar Nota</h1>
     </div>
 
+    <header>
+        <div class="container">
+            <div class="grid-container">
+                <div class="grid-item item1">Horário</div>
+                <div class="grid-item celula-blue amarelo">Segunda</div>
+                <div class="grid-item celula-blue amarelo">Terça</div>
+                <div class="grid-item celula-blue amarelo">Quarta</div>
+                <div class="grid-item celula-blue amarelo">Quinta</div>
+                <div class="grid-item celula-blue amarelo">Sexta</div>
+                <div class="grid-item celula-blue amarelo">7:30</div>
+                <div class="grid-item">8</div>
+                <div class="grid-item">9</div>
+                <div class="grid-item">10</div>
+                <div class="grid-item">11</div>
+                <div class="grid-item">12</div>
+                <div class="grid-item celula-blue amarelo ">8:10</div>
+                <div class="grid-item">14</div>
+                <div class="grid-item">15</div>
+                <div class="grid-item">16</div>
+                <div class="grid-item">17</div>
+                <div class="grid-item">18</div>
+                <div class="grid-item celula-blue amarelo">9:30</div>
+                <div class="grid-item">20</div>
+                <div class="grid-item">21</div>
+                <div class="grid-item">22</div>
+                <div class="grid-item">23</div>
+                <div class="grid-item">24</div>
+                <div class="grid-item celula-blue amarelo">10:20</div>
+                <div class="grid-item">26</div>
+                <div class="grid-item">27</div>
+                <div class="grid-item">28</div>
+                <div class="grid-item">29</div>
+                <div class="grid-item">30</div>
+                <div class="grid-item celula-blue amarelo">11:10</div>
+                <div class="grid-item">32</div>
+                <div class="grid-item">33</div>
+                <div class="grid-item">34</div>
+                <div class="grid-item">35</div>
+                <div class="grid-item">36</div>
+                <div class="grid-item celula-blue amarelo">13:20</div>
+                <div class="grid-item">38</div>
+                <div class="grid-item">39</div>
+                <div class="grid-item">40</div>
+                <div class="grid-item">41</div>
+                <div class="grid-item">42</div>
+                <div class="grid-item celula-blue amarelo">14:10</div>
+                <div class="grid-item">44</div>
+                <div class="grid-item">45</div>
+                <div class="grid-item">46</div>
+                <div class="grid-item">47</div>
+                <div class="grid-item">48</div>
+                <div class="grid-item celula-blue amarelo">15:20</div>
+                <div class="grid-item">50</div>
+                <div class="grid-item">51</div>
+                <div class="grid-item">52</div>
+                <div class="grid-item">53</div>
+                <div class="grid-item">54</div>
+                <div class="grid-item celula-blue amarelo">16:10</div>
+                <div class="grid-item">56</div>
+                <div class="grid-item">57</div>
+                <div class="grid-item">58</div>
+                <div class="grid-item">59</div>
+                <div class="grid-item">60</div>
+            </div>
+
+        </div>
+    </header>
 </body>
 
 </html>
